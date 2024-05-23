6501ad543b380a256caba3c4f18d88fa4a3142ef
caiovps <caiovps@debian.home>
2024-05-23 20:06:11 -0300

    Added SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SDL.h>
 
 struct S {
     int a;
@@ -11,6 +12,8 @@ int main() {
     struct S testCpp20 {
         .a = 10, .b = 20
     };
+
+    auto v = SDL_TRUE;
 
     return 0;
 }
